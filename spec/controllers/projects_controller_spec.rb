f4b44b51e2a300cf4b72afb3c078a18fd562bdfa
INO <ink.avc@gmail.com>
2025-01-11 10:31:57 +0900

    io-05-controllers Add テストケース ProjectsController show

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe ProjectsController, type: :controller do
       before do
         @user = FactoryBot.create(:user)
       end
-      fit "正常にレスポンスを返すこと" do
+      it "正常にレスポンスを返すこと" do
         sign_in @user
         get :index
         expect(response).to be_successful
       end
-      fit "200のレスポンスを返すこと" do
+      it "200のレスポンスを返すこと" do
         sign_in @user
         get :index
         expect(response).to have_http_status(200)
@@ -19,13 +19,44 @@ RSpec.describe ProjectsController, type: :controller do
     end
 
     context "ゲスト・ユーザーとして" do
-      fit "302レスポンスを返すこと" do
+      it "302レスポンスを返すこと" do
         get :index
         expect(response).to have_http_status(302)
       end
 
-      fit "サインイン画面にリダイレクトすること" do
+      it "サインイン画面にリダイレクトすること" do
         get :index
+        expect(response).to redirect_to("/users/sign_in")
+      end
+    end
+  end
+
+  describe "#show" do
+    before do
+      @user = FactoryBot.create(:user)
+      @project = FactoryBot.create(:project, owner: @user)
+    end
+    context "認証済みのユーザーとして" do
+      it "正常にレスポンスを返すこと" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to be_successful
+      end
+      it "200のレスポンスを返すこと" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "ゲスト・ユーザーとして" do
+      it "302レスポンスを返すこと" do
+        get :show, params: { id: @project.id }
+        expect(response).to have_http_status(302)
+      end
+
+      it "サインイン画面にリダイレクトすること" do
+        get :show, params: { id: @project.id }
         expect(response).to redirect_to("/users/sign_in")
       end
     end
