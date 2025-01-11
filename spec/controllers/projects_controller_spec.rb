d44044e9ada23f7d35e7eb2aa066b4f3d9f01bc1
INO <ink.avc@gmail.com>
2025-01-11 10:14:33 +0900

    io-05-controllers Adゲストユーザーの場合のテストケース

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   describe "#index" do
-    before do
-      @user = FactoryBot.create(:user)
+    context "認証済みのユーザーとして" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      fit "正常にレスポンスを返すこと" do
+        sign_in @user
+        get :index
+        expect(response).to be_successful
+      end
+      fit "200のレスポンスを返すこと" do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status(200)
+      end
     end
-    fit "正常にレスポンスを返すこと" do
-      sign_in @user
-      get :index
-      expect(response).to be_successful
-    end
-    fit "200のレスポンスを返すこと" do
-      sign_in @user
-      get :index
-      expect(response).to have_http_status(200)
+
+    context "ゲスト・ユーザーとして" do
+      fit "302レスポンスを返すこと" do
+        get :index
+        expect(response).to have_http_status(302)
+      end
+
+      fit "サインイン画面にリダイレクトすること" do
+        get :index
+        expect(response).to redirect_to("/users/sign_in")
+      end
     end
   end
 end
