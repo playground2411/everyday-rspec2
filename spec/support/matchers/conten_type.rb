c7e63a6869f3b458926105a6dfb30bdbd0c281cd
INO <ink.avc@gmail.com>
2025-01-18 12:30:02 +0900

    Update カスタムマッチャに例外処理を追加

--- spec/support/matchers/conten_type.rb
+++ spec/support/matchers/conten_type.rb
@@ -1,9 +1,16 @@
 RSpec::Matchers.define :have_content_type do |expected|
   match do |actual|
-    content_types = {
+    begin
+      actual.content_type.include? content_type(expected)
+    rescue ArgumentError
+      false
+    end
+  end
+  def content_type(type)
+    types = {
       html: "text/html",
       json: "application/json",
     }
-    actual.content_type.include? content_types[expected.to_sym]
+    types[type.to_sym] || "unknown content type"
   end
 end
