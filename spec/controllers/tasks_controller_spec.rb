12011bcc533b8ab47b8b71f15cecd7aa3a081dae
INO <ink.avc@gmail.com>
2025-01-18 12:15:38 +0900

    Add カスタムマッチャを使う

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe TasksController, type: :controller do
       sign_in user
       get :show, format: :json,
           params: { project_id: project.id, id: task.id }
-      expect(response.content_type).to include "application/json"
+      expect(response).to have_content_type :json
     end
   end
 
