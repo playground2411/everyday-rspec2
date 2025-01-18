34f59773269b5e2f3ea4c8f8b90c5cc7c2436348
INO <ink.avc@gmail.com>
2025-01-18 12:17:11 +0900

    Fix 認証のメソッドを利用することでによるリダイレクトへの対応

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe "Projects", type: :system do
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
 
-    sign_in_as user
+    sign_in user
+
+    visit root_path
 
     expect {
       click_link "New Project"
